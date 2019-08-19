d47efaacb62e4e1135f6571234b0fa44147af347
Lipi <lipijain.dav@gmail.com>
2019-08-19 20:24:11 +0530

    working
    python

--- even_odd.py
+++ even_odd.py
@@ -17,3 +17,17 @@ for j in my_list :
 print ("enter even number list\t", even_list)
 print ("enter odd number list\t", odd_list)
         
+# hi 
+# hi
+# ab zara try kar, iss progra ko save kar, gitub desktop khol aur
+# commit kar ke dekh
+# ha uss prr khol diya..phir?? 
+#uss par kis par? github destop 
+# ha, now clik on your repo, and see if it shows u the option to commit to mster at 
+# the bottom left
+# src dekh joi maine kal bheji thi
+# summary m kya doo ?? yaha ?? 
+# # aur description m ??
+# kuch bhi dede kuch bhi dede kuch bhi
+# nhi ho rha yaar uss prr click 
+# ek baar discord prr aana please, nahi aise hi save kar
